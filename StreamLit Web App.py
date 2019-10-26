261c7e79810386500252270b8d1a8dbeab4020c8
rishsol <rishabsolanki09@gmail.com>
2019-10-26 15:49:59 -0400

    Streamlit used to locally host visuals of sentiment of companies over time

--- StreamLit Web App.py
+++ StreamLit Web App.py
@@ -1,1 +1,55 @@
+import datetime
 import streamlit as st
+import nltk
+nltk.downloader.download('vader_lexicon')
+import warnings
+warnings.filterwarnings('ignore')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+
+sia = SentimentIntensityAnalyzer()
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import time
+
+sentiments = []
+news_list = []
+passage_list = []
+passage = ""
+company = st.text_input('Enter a publicly traded company')
+
+for i in range(1, 3):
+    page = urlopen('https://www.businesstimes.com.sg/search/' + company + '?page='+str(i)).read()
+    soup = BeautifulSoup(page, features="html.parser")
+    posts = soup.findAll("div", {"class": "media-body"})
+    for post in posts:
+        url = post.a['href']
+        try:
+            html_info = urlopen(url).read()
+            soup = BeautifulSoup(html_info)
+            sentences = soup.findAll("p")
+
+            for sentence in sentences:
+                passage += sentence.text
+
+            passage_list.append(passage)
+        except:
+            continue
+
+for passage in passage_list:
+    sentiment = sia.polarity_scores(passage)['compound']
+    sentiments.append(sentiment)
+
+sentiments.reverse()
+
+d = datetime.today()
+fig = plt.plot(range(len(sentiments)), sentiments)
+plt.title('Sentiment of Articles about ' + company.upper() + ' on "The Business Times" Over Time')
+plt.ylabel('Sentiment Index')
+plt.xlabel('Day')
+st.pyplot()
+
+st.write('Day 0 Represents 20 days since today, or ' + d - timedelta(days=20))
+
