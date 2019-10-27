5382bb129c3465cdcb6694d4ab195d3dc429d880
rishsol <rishabsolanki09@gmail.com>
2019-10-26 23:16:21 -0400

    Updated conclusions

--- StreamLit Web App.py
+++ StreamLit Web App.py
@@ -45,9 +45,10 @@ for passage in passage_list:
 sentiments.reverse()
 date_list.reverse()
 plt.plot(range(len(sentiments)), sentiments)
-plt.title('Sentiment of Articles about ' + company.upper() + ' on "The Business Times" Over Time')
+plt.title('Sentiment of Articles about ' + company.upper() + ' in "The Business Times" Over Time')
 plt.ylabel('Sentiment Index')
 plt.xlabel('Time Period')
+plt.xticks(range(len(sentiments)))
 plt.tight_layout()
 st.pyplot()
 
@@ -55,13 +56,29 @@ def sentimentStatement():
     tOld = date_list[0]
     tRecent = date_list[len(date_list) - 1]
     if(sentiments[0] > sentiments[len(sentiments) - 1]):
-        return 'Sentiment has decreased by ' + str(round(sentiments[0] - sentiments[len(sentiments) - 1], 5)) + ' from ' + tOld + ' to ' + tRecent
+        return 'Sentiment has decreased by ' + str(round(sentiments[0] - sentiments[len(sentiments) - 1], 5)) + ' points from ' + tOld + ' to ' + tRecent + ' for ' + company + '.'
     elif(sentiments[0] < sentiments[len(sentiments) - 1]):
-        return 'Sentiment has increased by ' + str(round(sentiments[len(sentiments) - 1] - sentiments[0], 5)) + ' from ' + tOld + ' to ' + tRecent
+        return 'Sentiment has increased by ' + str(round(sentiments[len(sentiments) - 1] - sentiments[0], 5)) + ' points from ' + tOld + ' to ' + tRecent + ' for ' + company + '.'
     else:
-        return 'Sentiment has not changed from ' + tOld + ' to ' + tRecent
+        return 'Sentiment has not changed from ' + tOld + ' to ' + tRecent + ' for ' + company + '.'
 
-st.write('Time Period 0 represents ' + date_list[0] + ' while the final time period (time period ' + str(len(sentiments)) + ') represents ' + date_list[len(date_list) - 1] + '. '
-         'A downward trend shows a decline in positive sentiment, while an upwards trend represents a rise in positive sentiment.')
+st.write('Each time period represents a date when "The Business Times" wrote an article about ' + company +
+         '. The first time period, time period 0, represents the oldest  article about ' + company + ' I was able to scrape. Here, time period 0 represents ' + date_list[0] +
+        '. The final time period (time period ' + str(len(sentiments) - 1) + ') represents the latest article on ' + company + ' and here, it represents ' + date_list[len(date_list) - 1] + '. I could not just'
+        ' add theses dates since the labels would not be able to fit within the graph otherwise. A downward trend shows a decline in positive sentiment, while an upwards trend represents a rise in positive sentiment.')
 st.write()
+
 st.write(sentimentStatement())
+st.write()
+
+difference = sentiments[0] - sentiments[len(sentiments) - 1]
+if(difference > 1 ):
+    st.write('Thus, company public opinion has declined rather significantly over the time period.')
+elif(difference < -1):
+    st.write('Thus, company public opinion has appreciated significantly over the time period.')
+elif(difference > 0.5):
+    st.write('Thus, there has been a decent decline in public opinion of the company over the time period.')
+elif(difference < -0.5):
+    st.write('Thus, there has been a decent rise in public opinion of the company over the time period.')
+else:
+    st.write('Thus, there has not been an appreciable change in public opinion of the company over the time period.')
